a97890e0a9cfcf805ad5215d7c4ff5afcb92d57a
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-07-13 16:20:31 +0900

    Write minimum expected specifications.

---
+++ spec/finder_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe CodeKeeper::Finder do
+  it 'caches paths and remove dupulicated one' do
+  end
+
+  it 'stores list of files' do
+  end
+
+  it 'returns list of files' do
+  end
+  # TODO: It needs to store results, which has keys as file names and values as points
+end
