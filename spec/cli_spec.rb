cab5d1f147670a3bcd66ea73b22e749791c90a3e
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-07-17 21:12:16 +0900

    Write minimum requirement of a cli class.

---
+++ spec/cli_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.describe CodeKeeper::Cli do
+  describe '.run' do
+    it 'returns Hash object which restores a file name as a key and score as a value' do
+    end
+  end
+end
