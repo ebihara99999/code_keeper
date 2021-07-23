1e974fac9aed73def7110041afe4b3acfbbe6c7b
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-07-23 13:21:49 +0900

    Fix trailing space.

--- spec/code_keeper/cli_spec.rb
+++ spec/code_keeper/cli_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CodeKeeper::Cli do
   describe '.run' do
     it 'returns String object which is formatted by CodeKeeper::Formatter' do
       result = CodeKeeper::Cli.run(['./spec/fixtures/branch_in_loop.rb', './spec/fixtures/target_sample.rb'])
-      expect(result).to be_a String 
+      expect(result).to be_a String
     end
   end
 end
