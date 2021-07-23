72c6b45223916de24121d0fbe17b091440458448
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-07-23 13:10:39 +0900

    Fix failing spec caused by changing an cli interface.

--- spec/code_keeper/cli_spec.rb
+++ spec/code_keeper/cli_spec.rb
@@ -2,9 +2,9 @@
 
 RSpec.describe CodeKeeper::Cli do
   describe '.run' do
-    it 'returns Hash object which restores a file name as a key and score as a value' do
+    it 'returns String object which is formatted by CodeKeeper::Formatter' do
       result = CodeKeeper::Cli.run(['./spec/fixtures/branch_in_loop.rb', './spec/fixtures/target_sample.rb'])
-      expect(result).to be_a Hash
+      expect(result).to be_a String 
     end
   end
 end
