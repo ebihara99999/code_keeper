eced119f63da7e3324ebebc4de34cea3459da091
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-07-30 17:52:07 +0900

    Add spec contents.

--- spec/code_keeper/finder_spec.rb
+++ spec/code_keeper/finder_spec.rb
@@ -28,17 +28,85 @@ RSpec.describe CodeKeeper::Finder do
   end
 
   context 'the argument includes a path to a flat directory' do
+    before do
+      @relative_paths = ['./spec/fixtures/flat_dir']
+    end
+
+    it 'stores relative paths' do
+      expected_paths = [
+        './spec/fixtures/flat_dir/a.rb',
+        './spec/fixtures/flat_dir/b.rb'
+      ]
+
+      finder = CodeKeeper::Finder.new(@relative_paths)
+      expect(finder.file_paths).to match_array(expected_paths)
+    end
   end
 
   context 'the argument includes a path to a nested directory' do
+    before do
+      @relative_paths = ['./spec/fixtures/nested_dir']
+    end
+
+    it 'stores relative paths' do
+      expected_paths = [
+        './spec/fixtures/nested_dir/sub_dir/a.rb',
+        './spec/fixtures/nested_dir/a.rb'
+      ]
+
+      finder = CodeKeeper::Finder.new(@relative_paths)
+      expect(finder.file_paths).to match_array(expected_paths)
+    end
   end
 
   context 'the argument includes two different directories' do
+    before do
+      @relative_paths = ['./spec/fixtures/nested_dir', './spec/fixtures/flat_dir']
+    end
+
+    it 'stores relative paths' do
+      expected_paths = [
+        './spec/fixtures/flat_dir/a.rb',
+        './spec/fixtures/flat_dir/b.rb',
+        './spec/fixtures/nested_dir/sub_dir/a.rb',
+        './spec/fixtures/nested_dir/a.rb'
+      ]
+
+      finder = CodeKeeper::Finder.new(@relative_paths)
+      expect(finder.file_paths).to match_array(expected_paths)
+    end
   end
 
   context 'the argument includes a parent directory and the sub directory' do
+    before do
+      @relative_paths = ['./spec/fixtures/nested_dir', './spec/fixtures/nested_dir/sub_dir']
+    end
+
+    it 'stores relative paths' do
+      expected_paths = [
+        './spec/fixtures/nested_dir/sub_dir/a.rb',
+        './spec/fixtures/nested_dir/a.rb'
+      ]
+
+      finder = CodeKeeper::Finder.new(@relative_paths)
+      expect(finder.file_paths).to match_array(expected_paths)
+    end
   end
 
   context 'the argument includes a directory and a file' do
+    before do
+      @relative_paths = ['./spec/fixtures/branch_in_loop.rb', './spec/fixtures/flat_dir']
+    end
+
+    it 'stores relative paths' do
+      expected_paths = [
+        './spec/fixtures/flat_dir/a.rb',
+        './spec/fixtures/flat_dir/b.rb',
+        './spec/fixtures/branch_in_loop.rb'
+      ]
+
+      finder = CodeKeeper::Finder.new(@relative_paths)
+      expect(finder.file_paths).to match_array(expected_paths)
+    end
   end
 end
