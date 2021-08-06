f8ce49963bd6b1ac2606f9609df439e55088d176
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-08-07 03:21:58 +0900

    Make the tests passed

--- lib/code_keeper/cli.rb
+++ lib/code_keeper/cli.rb
@@ -3,12 +3,28 @@
 module CodeKeeper
   # Offers cli interface and execute measurement.
   class Cli
+    SUCCESS_CODE = 0
+    GENERAL_ERROR_CODE = 1
+    ERROR_CODE = 2
+    INTERRUPTION_CODE = 128 + Signal.list['INT']
+
     def self.run(paths)
       result = {}
       Finder.new(paths).file_paths.each do |path|
         result[path] = CyclomaticComplexity.new(path).score
       end
-      ::CodeKeeper::Formatter.format(result)
+
+      puts ::CodeKeeper::Formatter.format(result)
+      SUCCESS_CODE
+    rescue Interrupt
+      puts 'Exiting...'
+      INTERRUPTION_CODE
+    rescue CodeKeeper::TargetFileNotFoundError => e
+      puts e.message
+      ERROR_CODE
+    rescue StandardError => e
+      puts e.message
+      GENERAL_ERROR_CODE
     end
   end
 end
