af3856c3be2b6bd3c921b28e036c40775f53dfaf
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-08-14 05:42:04 +0900

    Create the CodeKeeper::Scorer class.

--- lib/code_keeper/scorer.rb
+++ lib/code_keeper/scorer.rb
@@ -5,6 +5,16 @@ module CodeKeeper
   class Scorer
     class << self
       def keep(paths)
+        result = CodeKeeper::Result.new
+        metrics = result.scores.keys
+
+        Finder.new(paths).file_paths.each do |path|
+          metrics.each do |metric|
+            result.add(:cyclomatic_complexity, path, ::CodeKeeper::Metrics::MAPPINGS[metric].new(path).score)
+          end
+        end
+
+        result
       end
     end
   end
--- spec/code_keeper/scorer_spec.rb
+++ spec/code_keeper/scorer_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe CodeKeeper::Scorer do
   describe '.keep' do
-    it 'returns an instance of Result' do
+    it 'returns an instance of CodeKeeper::Result' do
       expect(CodeKeeper::Scorer.keep(['./spec/fixtures/branch_in_loop.rb'])).to be_a CodeKeeper::Result
     end
   end
