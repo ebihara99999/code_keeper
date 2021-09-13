5b3231be59a1e09dc7d4e5fac09c228a71d259d3
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-09-13 22:47:35 +0900

    Measure all metrics by default

--- lib/code_keeper/config.rb
+++ lib/code_keeper/config.rb
@@ -6,7 +6,7 @@ module CodeKeeper
     attr_accessor :metrics, :number_of_threads
 
     def initialize
-      @metrics = [:cyclomatic_complexity]
+      @metrics = [:cyclomatic_complexity, :class_length, :abc_metric]
       @number_of_threads = 2
     end
   end
