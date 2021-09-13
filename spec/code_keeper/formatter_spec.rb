866f65ab5aed95afded44229b7423c64300a20c7
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-09-13 22:00:53 +0900

    Write a new specification when specified a class length metric.

--- spec/code_keeper/formatter_spec.rb
+++ spec/code_keeper/formatter_spec.rb
@@ -2,29 +2,58 @@
 
 RSpec.describe CodeKeeper::Formatter do
   describe '.format' do
-    let(:expected_string) do
-      <<~EOS
-  Scores:
+    context 'it shows a filename' do
+      let(:expected_string) do
+        <<~EOS
+          Scores:
 
-  Metric: cyclomatic_complexity
-  Filename: /foo/bar/code_keeper/spec/fixtures/branch_in_loop.rb
-  Score: 2
-  ---
-  Metric: cyclomatic_complexity
-  Filename: /foo/bar/code_keeper/spec/fixtures/target_sample.rb
-  Score: 1
-  ---
-      EOS
+          Metric: cyclomatic_complexity
+          Filename: /foo/bar/code_keeper/spec/fixtures/branch_in_loop.rb
+          Score: 2
+          ---
+          Metric: cyclomatic_complexity
+          Filename: /foo/bar/code_keeper/spec/fixtures/target_sample.rb
+          Score: 1
+          ---
+        EOS
+      end
+
+      before do
+        @result = CodeKeeper::Result.new
+        @result.add(:cyclomatic_complexity, '/foo/bar/code_keeper/spec/fixtures/branch_in_loop.rb', 2)
+        @result.add(:cyclomatic_complexity, '/foo/bar/code_keeper/spec/fixtures/target_sample.rb', 1)
+      end
+
+      it 'returns an string' do
+        expect(CodeKeeper::Formatter.format(@result)).to eq expected_string
+      end
     end
 
-    before do
-      @result = CodeKeeper::Result.new
-      @result.add(:cyclomatic_complexity, '/foo/bar/code_keeper/spec/fixtures/branch_in_loop.rb', 2)
-      @result.add(:cyclomatic_complexity, '/foo/bar/code_keeper/spec/fixtures/target_sample.rb', 1)
-    end
+    context 'it shows a class name' do
+      let(:expected_string) do
+        <<~EOS
+          Scores:
 
-    it 'returns an string' do
-      expect(CodeKeeper::Formatter.format(@result)).to eq expected_string
+          Metric: class_length
+          Class: SimpleClass
+          Score: 2
+          ---
+          Metric: class_length
+          Class: NameSpaceClass
+          Score: 3
+          ---
+        EOS
+      end
+
+      before do
+        @result = CodeKeeper::Result.new
+        @result.add(:class_length, 'SimpleClass', 2)
+        @result.add(:class_length, 'NameSpaceClass', 3)
+      end
+
+      it 'returns an string' do
+        expect(CodeKeeper::Formatter.format(@result)).to eq expected_string
+      end
     end
   end
 end
