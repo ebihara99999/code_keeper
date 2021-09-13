698ffbad86c9f6f8a2ad46af93bef1e795b1a607
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-09-13 22:49:38 +0900

    Add mappings to other metrics.

--- lib/code_keeper/metrics.rb
+++ lib/code_keeper/metrics.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
+require 'code_keeper/metrics/abc_metric'
 require 'code_keeper/metrics/cyclomatic_complexity'
+require 'code_keeper/metrics/class_length'
 
 module CodeKeeper
   # Manage config values of metrics and the correspond classes
   module Metrics
-    MAPPINGS = { cyclomatic_complexity: CodeKeeper::Metrics::CyclomaticComplexity }.freeze
+    MAPPINGS = {
+      abc_metric: CodeKeeper::Metrics::AbcMetric,
+      cyclomatic_complexity: CodeKeeper::Metrics::CyclomaticComplexity,
+      class_length: CodeKeeper::Metrics::ClassLength
+    }.freeze
   end
 end
