33a7b523274110937e605d6a5207856fa75dab1d
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-09-13 22:39:00 +0900

    Modify implemention of the change of `#socre` interface.

--- lib/code_keeper/metrics/cyclomatic_complexity.rb
+++ lib/code_keeper/metrics/cyclomatic_complexity.rb
@@ -10,18 +10,20 @@ module CodeKeeper
       CONSIDERED_NODES = %i[if while until for csend block block_pass rescue when and or or_asgnand_asgn].freeze
 
       def initialize(file_path)
-        ps = Parser.parse(file_path)
+        @path = file_path
+        ps = Parser.parse(@path)
         @body = ps.ast
       end
 
       # returns score of cyclomatic complexity
       def score
-        @body.each_node(:lvasgn, *CONSIDERED_NODES).reduce(1) do |score, node|
+        final_score = @body.each_node(:lvasgn, *CONSIDERED_NODES).reduce(1) do |score, node|
           next score if !iterating_block?(node) || node.lvasgn_type?
           next score if node.csend_type? && discount_for_repeated_csend?(node)
 
           next 1 + score
         end
+        { "#{@path}": final_score }
       end
     end
   end
