b922dd2ec6c909f60da2fe8bf9219861a59b7c8d
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-09-15 02:52:57 +0900

    Add supported format configuration.

--- lib/code_keeper/config.rb
+++ lib/code_keeper/config.rb
@@ -3,11 +3,12 @@
 module CodeKeeper
   # Provide configuration
   class Config
-    attr_accessor :metrics, :number_of_threads
+    attr_accessor :metrics, :number_of_threads, :format
 
     def initialize
       @metrics = %i[cyclomatic_complexity class_length abc_metric]
       @number_of_threads = 2
+      @format = :json # json and csv are supported.
     end
   end
 end
