ce3ccffe6c6d9dce33ddcfd74a000cdb6c926f24
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-09-15 02:54:00 +0900

    Change formatter into supporting csv and json.

--- lib/code_keeper/formatter.rb
+++ lib/code_keeper/formatter.rb
@@ -1,32 +1,24 @@
 # frozen_string_literal: true
+require 'csv'
 
 module CodeKeeper
   # Format a result and make it human-readable.
   class Formatter
     class << self
       def format(result)
-        title = "Scores:\n\n"
-        formatted_result = +title
+        return result.scores.to_json if CodeKeeper.config.format == :json
 
+        # csv is supported besides json
+        csv_array = []
         result.scores.each_key do |metric|
-          result.scores[metric].each do |k, v|
-            klass_or_file_key = if metric == :class_length
-                                  'Class'
-                                else
-                                  'Filename'
-                                end
-
-            formatted_result.concat(
-              <<~EOS
-                Metric: #{metric}
-                #{klass_or_file_key}: #{k}
-                Score: #{v}
-                ---
-              EOS
-            )
-          end
+          result.scores[metric].each { |k, v| csv_array << [metric, k, v] }
         end
-        formatted_result
+
+        headers = %w[metric file score]
+        CSV.generate(headers: true) do |csv|
+          csv << headers
+          csv_array.each { |array| csv << array }
+        end
       end
     end
   end
