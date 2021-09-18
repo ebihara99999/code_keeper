49a6482b07ea270e42a1c03512001e408d7fc13b
Yusuke Ebihara <dev.biibiebi@gmail.com>
2021-09-19 00:12:04 +0900

    Change a namespace fixture to reflect bug cases.
    - The result doesn't show namespace.
    - Doesn't count module length.
    - RootClass length is wrong because there is a comment in inner nodes.
      - It's a bug of `#comment_line_count`.
    - It's double counting if there are two inner nodes or more.
      - It's a bug of `#line_count_of_inner_nodes`.

--- spec/fixtures/class_samples/namespace.rb
+++ spec/fixtures/class_samples/namespace.rb
@@ -1,20 +1,44 @@
-class NameSpaceClass
-  class A
-    a = 1
-    a = 2
-  end
+class RootClass
+  class NameSpaceClass
+    class A
+    # A comment
+      a = 1
+      a = 2
+    end
 
-  # This is NameSpaceClass's field.
-  n = 1
-  n = 2
-  n = 3
-  n = 4
+    # NameSpaceClass comment
+    n = 1
+    n = 2
+    n = 3
+    n = 4
+  end
+  # RootClass comment
+  r = 1
+  r = 2
+  r = 3
 end
 
-module NameSpaceModule
-  class B
-    b = 1
-    b = 2
-    b = 3
+module RootModule 
+  module NameSpaceModule
+    # NameSpaceModule comment
+    n = 1
+    n = 2
+    module B
+      # B comment
+      b = 1
+      b = 2
+      b = 3
+      b = 4
+    end
   end
+  # RootModule comment
+  r = 1
+  r = 2
+  r = 3
+end
+
+class C::D::E
+  # comment
+  e = 1
+  e = 2
 end
